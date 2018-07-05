2750357776cbbed8ea11eae8cec14926d60d1437
Abiodun <iabiodun3@ctisl.gtri.org>
2018-07-05 11:17:35 -0400

    made door connection send data to mqtt

--- nodes/door-node/door-connection.py
+++ nodes/door-node/door-connection.py
@@ -1,4 +1,5 @@
 from smbus import SMBus
+import paho.mqtt.client as mqtt
 import socket
 import time
 
@@ -6,7 +7,7 @@ bus = SMBus(1)
 ADDR = 0x06
 
 HOST = '192.168.4.1'
-PORT = 8000
+PORT = 1883
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
@@ -15,9 +16,11 @@ s.connect((HOST, PORT))
 def recv():
     return bus.read_byte(ADDR)
 
+client = mqtt.Client()
+client.connect(HOST, PORT, 60)
 
 while True:
     ac = recv()
     print(ac)
-    s.sendto(str(ac).encode(), (HOST, PORT))
+    client.publish("door/gyro", str(ac))
     time.sleep(1)
