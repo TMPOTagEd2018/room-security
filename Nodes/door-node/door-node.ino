bd3cc164aba386ce68a50c6df519cb58c38b256a
Simon Abrelat <simon.abrelat@gmail.com>
2018-07-05 15:55:26 -0400

    Working Door Node

---
+++ Nodes/door-node/door-node.ino
@@ -0,0 +1,76 @@
+#include <Wire.h>
+#include <math.h>
+
+#include <SparkFunLSM9DS1.h>
+
+LSM9DS1 imu;
+
+#define LSM9DS1_M  0x1E
+#define LSM9DS1_AG  0x6B
+
+#define ADDR 0x06
+#define DT 100
+
+const int in = 13;
+
+int openned = 0;
+int ang = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(2000);
+  Serial.println();
+  Serial.println("Hello");
+
+  Wire.begin(ADDR);
+  Wire.onReceive(recv);
+  Wire.onRequest(send);
+  
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+
+  if (!imu.begin()) {
+    Serial.println("GYRO NOT FOUND");
+    while(1);
+  }
+  Serial.println("Gyro found!");
+
+  pinMode(in, INPUT);
+
+  Serial.print("I2c opening on addr: ");
+  Serial.println(ADDR);
+  Serial.println("Ready ...");
+
+}
+
+
+
+void loop() { 
+  if(imu.gyroAvailable()){
+    imu.readGyro();
+  }
+
+  ang = constrain(imu.calcGyro(imu.gy), -127, 127);
+  Serial.print("Gyro: ");
+  Serial.println(ang);
+
+  openned = (digitalRead(in)) ? 1 : 0;
+  Serial.print("Openned: ");
+  Serial.println(openned);
+  
+  delay(DT);
+}
+
+void recv(int){
+  byte r = Wire.read();
+  Serial.println(r);
+}
+
+void send(){
+  //Wire.write(0x00 << 1);
+  Wire.write(ang);
+  //Wire.write(openned);
+}
+
