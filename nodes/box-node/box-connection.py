5356df308550f63e2a0de4bb1f4ddb98a1ef796f
Simon Abrelat <simon.abrelat@gmail.com>
2018-07-05 15:55:53 -0400

    Working Door Node

--- nodes/box-node/box-connection.py
+++ nodes/box-node/box-connection.py
@@ -1,4 +1,5 @@
 from smbus import SMBus
+import paho.mqtt.publish as p_mqtt
 import socket
 import time
 
@@ -6,18 +7,16 @@ bus = SMBus(1)
 ADDR = 0x06
 
 HOST = '192.168.4.1'
-PORT = 8000
+PORT = 1883
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((HOST, PORT))
 
 def recv():
     return bus.read_byte(ADDR)
 
-
 while True:
     ac = recv()
     print(ac)
-    s.sendto(str(ac).encode(), (HOST, PORT))
+    p_mqtt.single("door/gyro", str(ac), qos=1, hostname=HOST, port=PORT)
     time.sleep(1)
