82dc92c2d68bf7da3c5fd799edc601e06d37384a
Sidhesh Desai <sidheshdesaidev@gmail.com>
2018-07-09 14:16:47 -0400

    ssl for client

---
+++ nodes/sslclient.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as paho
+import paho.mqtt.publish as pub
+import paho.mqtt.subscribe as sub
+#not really using these two here but i jsut put them
+import socket
+import time
+
+broker = "Taged"
+#broker is the mothernode
+#it should have server.crt
+#idk how it's gonna recognize based on the certs
+
+
+HOST = '192.168.4.1'
+PORT = 8883
+
+node1 = paho.Client("node1")
+node1.tls_set('')  #<< Put CA certificate from raspi's here
+def on_connect:
+    global flag
+    flag = True
+node1.on_connect = on_connect
+node1.connect(broker, PORT)
+while not flag: 
+    node1.loop
+    time.sleep(1)
+node.pub("")
