b28fe960079f883b5eda81333276109bbf0b8932
Abiodun <iabiodun3@ctisl.gtri.org>
2018-07-13 10:26:40 -0400

    renamed ADDR to addr

--- nodes/box-node/box-connection.py
+++ nodes/box-node/box-connection.py
@@ -3,5 +3,5 @@ from node import Node
 box = Node("box", 0x06)
 box.register("contact", 0, lambda x: x)
 box.register("accel", 1, lambda x: x if x < 128 else x - 256)
-box.pipe("server", "alarm", lambda m: box.bus.write_byte(box.ADDR, int(m.payload)))
+box.pipe("server", "alarm", lambda m: box.bus.write_byte(box.addr, int(m.payload)))
 box.start()
