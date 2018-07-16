c49f296a7a5415b2a337e160daa0c9a24c6914cb
Simon Abrelat <simon.abrelat@gmail.com>
2018-07-16 14:37:39 -0400

    Working heartbeat, python3.6, and more elegant i2c error handling

--- nodes/node.py
+++ nodes/node.py
@@ -3,7 +3,7 @@ import random
 import time
 
 import paho.mqtt.client as paho
-from smbus2 import SMBus
+import smbus2
 
 
 class Sensor:
@@ -27,8 +27,8 @@ class Node:
     def __init__(self, name, addr):
         self.name = name
         self.addr = addr
-        # self.bus = SMBus(1)
-        self.c = paho.Client("name")
+        self.bus = smbus2.SMBus(1)
+        self.c = paho.Client(str(name))
         self.c.tls_set("ca.crt")
         self.c.connect("192.168.4.1", 8883)
 
@@ -46,9 +46,9 @@ class Node:
 
         self.c.on_message = handler
         self.c.subscribe("server/key")
-        self.c.publish(f"{self.name}/key", payload=public_key, qos=1)
+        self.c.publish(self.name + "/key", payload=public_key, qos=1)
 
-        print(f"Sent my public key: {public_key}")
+        print("Sent my public key: " + str(public_key))
         print("Waiting for their public key...")
 
         start_time = time.time()
@@ -62,14 +62,14 @@ class Node:
 
         server_public_key = int(server_public_key)
 
-        print(f"Received server public key: {server_public_key}")
+        print("Received server public key: " + str(server_public_key))
 
         self.c.on_message = None
         self.c.unsubscribe("server/key")
 
         sk = dh.gen_shared_key(server_public_key)
 
-        print(f"Key exchange completed with server, shared key {sk}")
+        print("Key exchange completed with server, shared key " + sk)
 
         random.seed(int(sk, 16))
 
@@ -85,6 +85,7 @@ class Node:
 
     def start(self):
         counter = 0
+        fails = 0
 
         while not self.exchange():
             pass
@@ -92,11 +93,22 @@ class Node:
         print("Beginning sensor loop.")
 
         while True:
-            d = self.bus.read_i2c_block_data(self.addr, 48)
+            with smbus2.SMBusWrapper(1) as bw:
+                try:
+                    d = bw.read_i2c_block_data(self.addr, 0, len(self.sensors))
+                    fails = 0
+                except:
+                    fails = fails + 1
+                    if fails >= 10:
+                        self.c.publish(self.name + "/heartbeat", -1, qos=2)
+                        print("EXITTING DUE TO I2C ERROR")
+                        break
+                    pass
+
             for i in self.inputs:
                 r = self.c.subscribe(i.name + "/" + i.topic, qos=1)
-                i.function(r)
-                print("Recieved: ", bytes(r.payload))
+                v = i.function(r)
+                print("Recieved: ", bytes(v))
             for s in self.sensors:
                 data = s.function(d[s.place])
                 print((self.name + "/" + s.name), data)
@@ -107,7 +119,7 @@ class Node:
             if counter == 4:
                 print("Sending heartbeat.")
                 random.setstate(self.rng)
-                self.c.publish(f"{self.name}/heartbeat",
+                self.c.publish(self.name + "/heartbeat",
                                random.getrandbits(32), qos=2)
                 self.rng = random.getstate()
                 counter = 0
